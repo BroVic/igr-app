059660d5b9568250376329343529779a45d1ad20
BroVic <victorordu@outlook.com>
2016-06-03 19:22:35 +0100

    Non-descript changes
    Got nuthin' to say now.

--- igr-app/server.R
+++ igr-app/server.R
@@ -9,7 +9,7 @@ igrbank <- readRDS(file = "data/igr-tidy.rds")
 shinyServer(function(input, output) {
 
   dataInput <- reactive({
-      revenues(igrbank, office = input$state )
+      revenues(igrbank, office = input$state ) # source code at helpers.R
     })
   
   names.arg <- levels(igrbank$revenue.cat)
@@ -17,21 +17,12 @@ shinyServer(function(input, output) {
   
   output$chart1 <- renderPlot({
     par(mar = c(12, 6, 4, 5) + 0.2)
-    barplot(dataInput()/1000,
-            ylim = c(0, max(dataInput()/1000)*1.25),
-            xaxt = "n",
-            xlab = "",
-            yaxt = "n",
-            ylab = "",
-            col = "red",
+    barplot(dataInput()/1000, ylim = c(0, max(dataInput()/1000)*1.25), xaxt = "n",
+            xlab = "", yaxt = "n", ylab = "", col = "red",
             main = paste("Revenue from", input$state))
     axis(2, las = 2)
-    text(seq(from = 1, to = len, by = 1),
-         par("usr")[3] - 0.5,
-         srt = 45,
-         adj = 1,
-         labels = names.arg,
-         xpd = TRUE)
+    text(seq(from = 1, to = len, by = 1), par("usr")[3] - 0.5, srt = 45, adj = 1,
+         labels = names.arg, xpd = TRUE)
     grid(NA, ny = NULL, col = "gray", lty = "dashed")
     mtext(1, text = "Payment Categories", line = 11)
     mtext(2, text = "Amount (NGN '000)", line = 5)
